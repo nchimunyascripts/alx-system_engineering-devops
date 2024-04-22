ed15eed025ad7d750c6aa2172f39a8896cd0bf02
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-04-23 00:35:00 +0200

    sample

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp with specific requirment
 
-file { 'holberton':
-  path    => '/tmp/holberton',
+file { 'school':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
